bf481d1f95b788143403c035312f5489edd28819
AriGabay <arigabay19988@gmail.com>
2021-06-28 22:02:09 +0300

    log env vars

--- models/index.js
+++ models/index.js
@@ -7,7 +7,11 @@ const basename = path.basename(__filename);
 const db = {};
 const dotenv = require('dotenv');
 dotenv.config();
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.PASSWORD, {
+const envVars = Object.keys(process.env)
+  .map((key) => `${key}:${process.env[key]};`)
+  .reduce((lastVal, currVal) => lastVal + currVal, '');
+console.log(envVars);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
   dialectOptions: {
