fb8b8dac87412d2c18b18f025b8ae0dccead8050
AriGabay <arigabay19988@gmail.com>
2021-06-30 04:04:15 +0300

    Fix Cors1

--- server.js
+++ server.js
@@ -32,7 +32,25 @@ const logger = require('./services/logger.service');
 
 const app = express();
 const http = require('http').createServer(app);
-app.use(cors());
+// app.use(cors({ origin: true }));
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   next();
