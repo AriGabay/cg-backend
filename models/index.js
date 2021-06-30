c47642c496b9455163802077e7217bb235ba9c3a
AriGabay <arigabay19988@gmail.com>
2021-06-30 15:42:21 +0300

    fix ssl

--- models/index.js
+++ models/index.js
@@ -14,7 +14,6 @@ console.log(envVars);
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
-  ssl: true,
   dialectOptions: {
     ssl: true,
   },
