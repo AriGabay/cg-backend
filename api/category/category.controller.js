0c90069860091fa552028aeac720323c87050af9
AriGabay <arigabay19988@gmail.com>
2021-07-05 02:54:16 +0300

    add console logs

--- api/category/category.controller.js
+++ api/category/category.controller.js
@@ -4,19 +4,25 @@ class CategoryController {
   }
   getCategory = async (req, res) => {
     try {
+      console.log('get Category start controller');
+      console.log('req.query:', req.query);
       const { include, ...query } = req.query;
       const categories = await this.categoryService.getCategories({ ...query }, include ?? false);
-      if (categories) {
+      if (categories && categories.length) {
+        console.log('categories:', categories);
         res.send(categories);
       } else {
         throw Error('No categories found');
       }
     } catch (error) {
-      res.status(200).send({ error: true, message: error?.message ?? error });
+      res.status(404).send({ error: true, message: error?.message ?? error });
     }
+    console.log('getCategory end controller');
   };
   createCategory = async (req, res) => {
     try {
+      console.log('create Category start controller');
+      console.log('req.body:', req.body);
       const category = await this.categoryService.createCategory(req.body);
       if (category) {
         res.send(category);
@@ -26,25 +32,31 @@ class CategoryController {
     } catch (error) {
       res.status(404).send({ error: true, message: error?.message ?? error });
     }
+    console.log('create Category end controller');
   };
   removeCategory = async (req, res) => {
     try {
-      const id = req.params;
+      console.log('remove Category start controller');
+      const id = req.params.id;
+      console.log('id:', id);
       const categories = await this.categoryService.removeCategory(id);
       if (categories === 1) {
         res.send(`success remove category id : ${id}`);
-        // res.send(`success remove category id :${id}`);
       } else {
         throw Error('Can not remove category');
       }
     } catch (error) {
       res.status(404).send({ error: true, message: error?.message ?? error });
     }
+    console.log('remove Category end controller');
   };
   updateCategory = async ({ body }, res) => {
     try {
+      console.log('update Category start controller');
       const id = body.id;
       const displayName = { displayName: body.displayName };
+      console.log('id:', id);
+      console.log('displayName:', displayName);
       const category = await this.categoryService.updateCategory(id, displayName);
       if (category && category.length) {
         res.send(`success update category id : ${id} to Display Name : ${body.displayName}`);
@@ -54,6 +66,7 @@ class CategoryController {
     } catch (error) {
       res.status(404).send({ error: true, message: error?.message ?? error });
     }
+    console.log('update Category end controller');
   };
 }
 module.exports = CategoryController;
--- api/category/category.service.js
+++ api/category/category.service.js
@@ -3,10 +3,12 @@ const db = require('../../models/index');
 class CategoryService {
   async createCategory(body) {
     try {
+      console.log('create category start service');
       console.log('body:', body);
       const query = undefined;
       const include = true;
       const data = await this.getCategories(query, include);
+      console.log('get category', data);
       data.map((element) => {
         if (element.displayName === body.displayName) {
           throw Error('cannot add category');
@@ -21,25 +23,31 @@ class CategoryService {
     } catch (error) {
       console.error({ error: true, message: error?.message ?? error });
     }
+    console.log('create category end service');
   }
 
   async getCategories(query, include = false) {
     try {
-      console.log('include:', include);
+      console.log('get categories start service');
       if (include === 'true') include = true;
       if (include === 'false') include = false;
       const includeConfig = { all: include, nested: include };
+      console.log('query:', query);
+      console.log('include:', include);
       return await db.Category.findAll({ where: { ...query }, include: include ? includeConfig : undefined }).catch(
         (err) => {
-          console.log('err:', err);
+          console.error('err:', err);
         }
       );
     } catch (error) {
       console.error({ error: true, message: error?.message ?? error });
     }
+    console.log('get categories end service');
   }
 
   async updateCategory(id, displayName) {
+    console.log('update categories start service');
+    console.log('id, displayName:', id, displayName);
     return await db.Category.update(
       { ...displayName },
       {
@@ -50,7 +58,9 @@ class CategoryService {
     );
   }
 
-  async removeCategory({ id }) {
+  async removeCategory(id) {
+    console.log('remove category start service');
+    console.log('id to remove : ', id);
     return await db.Category.destroy({
       where: {
         id,
